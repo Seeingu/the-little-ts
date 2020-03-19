def439e667acb51f18b4b4428478538c2b6df5d6
Seeingu <seeingasu@gmail.com>
2020-03-19 17:02:27 +0800

    chap.3

--- src/__tests__/util.spec.ts
+++ src/__tests__/util.spec.ts
@@ -1,4 +1,21 @@
-import { car, isEq, cdr, cons, or, isMember } from '../util';
+import {
+  car,
+  isEq,
+  cdr,
+  cons,
+  or,
+  isMember,
+  rember,
+  multirember,
+  firsts,
+  insertR,
+  multiinsertR,
+  insertL,
+  multiinsertL,
+  subst,
+  multisubst,
+  subst2
+} from '../util';
 import { SchemeConstants } from '../consts';
 
 const nonLists = ['a', 123];
@@ -128,6 +145,382 @@ describe('member', () => {
     ];
     falsyPairs.forEach(pair => {
       expect(isMember(pair.atom, pair.list)).toBeFalsy();
+    });
+  });
+});
+
+describe('rember', () => {
+  test('rember should works', () => {
+    const pairs = [
+      {
+        atom: 'a',
+        lat: [],
+        expected: []
+      },
+      {
+        atom: 'a',
+        lat: ['a', 'b', 'c'],
+        expected: ['b', 'c']
+      },
+      {
+        atom: 'a',
+        lat: ['b', 'c', 'd'],
+        expected: ['b', 'c', 'd']
+      },
+      {
+        atom: 'a',
+        lat: [['a'], 'b', 'c'],
+        expected: [['a'], 'b', 'c']
+      },
+      {
+        atom: 'a',
+        lat: ['a', 'b', 'a'],
+        expected: ['b', 'a']
+      }
+    ];
+    pairs.forEach(pair => {
+      expect(rember(pair.atom, pair.lat)).toEqual(pair.expected);
+    });
+  });
+
+  test('multirember should works', () => {
+    const pairs = [
+      {
+        atom: 'a',
+        lat: [],
+        expected: []
+      },
+      {
+        atom: 'a',
+        lat: ['a', 'b', 'c'],
+        expected: ['b', 'c']
+      },
+      {
+        atom: 'a',
+        lat: ['b', 'c', 'd'],
+        expected: ['b', 'c', 'd']
+      },
+      {
+        atom: 'a',
+        lat: [['a'], 'b', 'c'],
+        expected: [['a'], 'b', 'c']
+      },
+      {
+        atom: 'a',
+        lat: ['a', 'b', 'a', 'c', 'a'],
+        expected: ['b', 'c']
+      }
+    ];
+    pairs.forEach(pair => {
+      expect(multirember(pair.atom, pair.lat)).toEqual(pair.expected);
+    });
+  });
+});
+
+describe('first', () => {
+  test('first should works', () => {
+    const pairs = [
+      {
+        list: [
+          ['a', 'b'],
+          ['c', 'd'],
+          ['e', 'f']
+        ],
+        expected: ['a', 'c', 'e']
+      },
+      {
+        list: [[['a']], ['b', 'c'], ['d', 'e']],
+        expected: [['a'], 'b', 'd']
+      }
+    ];
+    pairs.forEach(pair => {
+      expect(firsts(pair.list)).toEqual(pair.expected);
+    });
+  });
+});
+
+describe('insertR', () => {
+  test('insertR should works', () => {
+    const pairs = [
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: [],
+        expected: []
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['b', 'c', 'd'],
+        expected: ['b', 'a', 'c', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'c',
+        lat: ['b', 'c', 'd', 'c'],
+        expected: ['b', 'c', 'a', 'd', 'c']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['c', 'd', 'e'],
+        expected: ['c', 'd', 'e']
+      }
+    ];
+    pairs.forEach(pair => {
+      expect(insertR(pair.newAtom, pair.oldAtom, pair.lat)).toEqual(
+        pair.expected
+      );
+    });
+  });
+
+  test('multiinsertR should works', () => {
+    const pairs = [
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: [],
+        expected: []
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['b', 'c', 'd'],
+        expected: ['b', 'a', 'c', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'c',
+        lat: ['b', 'c', 'd'],
+        expected: ['b', 'c', 'a', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['c', 'd', 'e'],
+        expected: ['c', 'd', 'e']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['b', 'c', 'b', 'd'],
+        expected: ['b', 'a', 'c', 'b', 'a', 'd']
+      }
+    ];
+    pairs.forEach(pair => {
+      expect(multiinsertR(pair.newAtom, pair.oldAtom, pair.lat)).toEqual(
+        pair.expected
+      );
+    });
+  });
+});
+
+describe('insertL', () => {
+  test('insertL should works', () => {
+    const pairs = [
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: [],
+        expected: []
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['b', 'c', 'd'],
+        expected: ['a', 'b', 'c', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'c',
+        lat: ['b', 'c', 'd', 'c'],
+        expected: ['b', 'a', 'c', 'd', 'c']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['c', 'd', 'e'],
+        expected: ['c', 'd', 'e']
+      }
+    ];
+    pairs.forEach(pair => {
+      expect(insertL(pair.newAtom, pair.oldAtom, pair.lat)).toEqual(
+        pair.expected
+      );
+    });
+  });
+  test('multiinsertL should works', () => {
+    const pairs = [
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: [],
+        expected: []
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['b', 'c', 'd'],
+        expected: ['a', 'b', 'c', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'c',
+        lat: ['b', 'c', 'd'],
+        expected: ['b', 'a', 'c', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['c', 'd', 'e'],
+        expected: ['c', 'd', 'e']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['b', 'c', 'b', 'd'],
+        expected: ['a', 'b', 'c', 'a', 'b', 'd']
+      }
+    ];
+    pairs.forEach(pair => {
+      expect(multiinsertL(pair.newAtom, pair.oldAtom, pair.lat)).toEqual(
+        pair.expected
+      );
+    });
+  });
+});
+
+describe('subst', () => {
+  test('subst should works', () => {
+    const pairs = [
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: [],
+        expected: []
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['b', 'c', 'd'],
+        expected: ['a', 'c', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'c',
+        lat: ['b', 'c', 'd'],
+        expected: ['b', 'a', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['c', 'd', 'e'],
+        expected: ['c', 'd', 'e']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['b', 'c', 'b', 'd'],
+        expected: ['a', 'c', 'b', 'd']
+      }
+    ];
+    pairs.forEach(pair => {
+      expect(subst(pair.newAtom, pair.oldAtom, pair.lat)).toEqual(
+        pair.expected
+      );
+    });
+  });
+
+  test('subst2 should works', () => {
+    const pairs = [
+      {
+        newAtom: 'a',
+        oldAtom1: 'b',
+        oldAtom2: 'b',
+        lat: [],
+        expected: []
+      },
+      {
+        newAtom: 'a',
+        oldAtom1: 'b',
+        oldAtom2: 'c',
+        lat: ['b', 'c', 'd'],
+        expected: ['a', 'c', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom1: 'c',
+        oldAtom2: 'd',
+        lat: ['b', 'c', 'd'],
+        expected: ['b', 'a', 'd']
+      },
+      // oldAtom2 occur before oldAtom1
+      {
+        newAtom: 'a',
+        oldAtom1: 'd',
+        oldAtom2: 'c',
+        lat: ['b', 'c', 'd'],
+        expected: ['b', 'a', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom1: 'b',
+        oldAtom2: 'f',
+        lat: ['c', 'd', 'e'],
+        expected: ['c', 'd', 'e']
+      },
+      {
+        newAtom: 'a',
+        oldAtom1: 'b',
+        oldAtom2: 'c',
+        lat: ['b', 'c', 'b', 'b', 'e'],
+        expected: ['a', 'c', 'b', 'b', 'e']
+      }
+    ];
+    pairs.forEach(pair => {
+      expect(
+        subst2(pair.newAtom, pair.oldAtom1, pair.oldAtom2, pair.lat)
+      ).toEqual(pair.expected);
+    });
+  });
+
+  test('multisubst should works', () => {
+    const pairs = [
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: [],
+        expected: []
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['b', 'c', 'd'],
+        expected: ['a', 'c', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'c',
+        lat: ['b', 'c', 'd'],
+        expected: ['b', 'a', 'd']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['c', 'd', 'e'],
+        expected: ['c', 'd', 'e']
+      },
+      {
+        newAtom: 'a',
+        oldAtom: 'b',
+        lat: ['b', 'c', 'b', 'b', 'e'],
+        expected: ['a', 'c', 'a', 'a', 'e']
+      }
+    ];
+    pairs.forEach(pair => {
+      expect(multisubst(pair.newAtom, pair.oldAtom, pair.lat)).toEqual(
+        pair.expected
+      );
     });
   });
 });
--- src/util.ts
+++ src/util.ts
@@ -36,3 +36,115 @@ export const isMember = (atom: unknown, list: unknown): boolean => {
     return isMember(atom, cdr(list));
   }
 };
+
+export const rember = (atom: unknown, lat: unknown): unknown => {
+  if (isNull(lat)) return lat;
+  if (isEq(car(lat), atom)) {
+    return cdr(lat);
+  }
+  return cons(car(lat), rember(atom, cdr(lat)));
+};
+
+export const multirember = (atom: unknown, lat: unknown): unknown => {
+  if (isNull(lat)) return lat;
+  if (isEq(car(lat), atom)) {
+    return multirember(atom, cdr(lat));
+  }
+  return cons(car(lat), multirember(atom, cdr(lat)));
+};
+
+export const firsts = (list: unknown): unknown => {
+  if (isNull(list)) return list;
+  return cons(car(car(list)), firsts(cdr(list)));
+};
+
+export const insertR = (
+  newAtom: unknown,
+  oldAtom: unknown,
+  lat: unknown
+): unknown => {
+  if (isNull(lat)) return lat;
+  if (isEq(oldAtom, car(lat))) {
+    return cons(car(lat), cons(newAtom, cdr(lat)));
+  }
+  return cons(car(lat), insertR(newAtom, oldAtom, cdr(lat)));
+};
+
+export const multiinsertR = (
+  newAtom: unknown,
+  oldAtom: unknown,
+  lat: unknown
+): unknown => {
+  if (isNull(lat)) return lat;
+  if (isEq(oldAtom, car(lat))) {
+    return cons(
+      car(lat),
+      cons(newAtom, multiinsertR(newAtom, oldAtom, cdr(lat)))
+    );
+  }
+  return cons(car(lat), multiinsertR(newAtom, oldAtom, cdr(lat)));
+};
+
+export const insertL = (
+  newAtom: unknown,
+  oldAtom: unknown,
+  lat: unknown
+): unknown => {
+  if (isNull(lat)) return lat;
+  if (isEq(oldAtom, car(lat))) {
+    return cons(newAtom, lat);
+  }
+  return cons(car(lat), insertL(newAtom, oldAtom, cdr(lat)));
+};
+
+export const multiinsertL = (
+  newAtom: unknown,
+  oldAtom: unknown,
+  lat: unknown
+): unknown => {
+  if (isNull(lat)) return lat;
+  if (isEq(oldAtom, car(lat))) {
+    return cons(
+      newAtom,
+      cons(car(lat), multiinsertL(newAtom, oldAtom, cdr(lat)))
+    );
+  }
+  return cons(car(lat), multiinsertL(newAtom, oldAtom, cdr(lat)));
+};
+
+export const subst = (
+  newAtom: unknown,
+  oldAtom: unknown,
+  lat: unknown
+): unknown => {
+  if (isNull(lat)) return lat;
+  if (isEq(oldAtom, car(lat))) {
+    return cons(newAtom, cdr(lat));
+  }
+  return cons(car(lat), subst(newAtom, oldAtom, cdr(lat)));
+};
+
+export const subst2 = (
+  newAtom: unknown,
+  oldAtom1: unknown,
+  oldAtom2: unknown,
+  lat: unknown
+): unknown => {
+  if (isNull(lat)) return lat;
+  if (or(isEq(oldAtom1, car(lat)), isEq(oldAtom2, car(lat)))) {
+    return cons(newAtom, cdr(lat));
+  }
+  return cons(car(lat), subst2(newAtom, oldAtom1, oldAtom2, cdr(lat)));
+};
+
+export const multisubst = (
+  newAtom: unknown,
+  oldAtom: unknown,
+  lat: unknown
+): unknown => {
+  if (isNull(lat)) return lat;
+  if (isEq(oldAtom, car(lat))) {
+    return cons(newAtom, multisubst(newAtom, oldAtom, cdr(lat)));
+  }
+  return cons(car(lat), multisubst(newAtom, oldAtom, cdr(lat)));
+};
